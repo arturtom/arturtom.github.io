b70ef809e9dd4bcecca1b147d51b1a17de0cfb8f
ArturTom <arttomas94@gmail.com>
2019-07-01 21:36:12 +0200

    add sass, make animation

--- navigation_panel.js
+++ navigation_panel.js
@@ -7,15 +7,60 @@ var subPageToScroll = new Map();
 window.addEventListener('scroll', function() {
     changeNavigationBarBackground();
     changeNavigationBarFont();
+    addAnimationClassToStartAnimation();
 })
 
 window.addEventListener('load', function() {
-    setTimeout(function() { setUpSubPageToScrollMapForChangingNavigationColors() }, 1000)
+    setTimeout(function() {
+        setUpSubPageToScrollMapForChangingNavigationColors()
+    }, 1000)
 })
+
+function addAnimationClassToStartAnimation(id) {
+    var scrollValue = window.scrollY;
+    var id;
+    for (let [k, v] of subPageToScroll) {
+        if ((scrollValue + 1) < (v - 400)) {
+            id = k;
+            break;
+        } else {
+            continue;
+        }
+    }
+    var subPage = getSubPageFromSubPages(id);
+    Array.from(subPage.children).forEach(function(child) {
+        addAnimationClassAndChangeDisplayOnBlock(child);
+    })
+}
+
+function addAnimationClassAndChangeDisplayOnBlock(child) {
+    child.classList.add('animation');
+    if (child.id == 'tea_article') {
+        child.style.display = 'block';
+    }
+    if (child.id == 'quality_article') {
+        Array.from(child.children).forEach(function(childParagraph) {
+            childParagraph.style.display = 'inline-flex';
+        })
+    } else if (child.id == 'goodness_article') {
+
+    } else {
+        Array.from(child.children).forEach(function(childParagraph) {
+            childParagraph.style.display = 'block';
+        })
+    }
+}
+
+function getSubPageFromSubPages(id) {
+    for (i = 0; i < subPages.length; i++) {
+        if (subPages[i].id == id) {
+            return subPages[i];
+        }
+    }
+}
 
 function changeNavigationBarFont() {
     var scrollValue = window.scrollY;
-    console.log(scrollValue);
     var id;
     for (let [k, v] of subPageToScroll) {
         if ((scrollValue + 1) < v) {
@@ -25,6 +70,7 @@ function changeNavigationBarFont() {
             continue;
         }
     }
+
     navigationSubPageOptions.forEach(function(el) {
         el.style.fontSize = '1rem';
         el.style.fontWeight = 'normal';
@@ -37,9 +83,12 @@ function changeNavigationBarFont() {
 function changeNavigationBarBackground() {
     var scrollValue = window.scrollY;
     if (scrollValue >= epilobiumImage.clientHeight) {
-        navigationBar.style.background = 'rgb(154, 156, 150, 1)';
+        navigationBar.style.background = 'rgb(154, 156, 150, .9)';
+        navigationBar.style.borderBottom = '2px solid darkgoldenrod';
     } else {
         navigationBar.style.background = 'rgb(154, 156, 150, .6)';
+        navigationBar.style.borderBottom = 'none';
+        navigationBar.style.height = '50px';
     }
 }
 
